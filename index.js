c0fca74839a2a88ec8070a00f8508fb53481253d
wpredoy <rah415704@gmail.com>
2020-10-18 12:05:32 +0600

    done my work

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ app.use(bodyParser.json());
 app.use(express.static('agency'));
 app.use(fileUpload());
 
-app.get('/', (req,res)=> {
-    res.send("working is done")
-})
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -32,29 +30,29 @@ client.connect(err => {
     const reviewCollection = client.db("name").collection("review");
     const adminEmailCT = client.db("name").collection("adminemail");
 
-// admin post
-app.post('/agencyinfo', (req, res) => {
-    const title = req.body.title;
-    const description = req.body.description;
-    const newImg = req.files.file.data;
-    const encImg = newImg.toString('base64');
-    var img = {
-        contentType: req.files.file.mimetype,
-        size: req.files.file.size,
-        img: Buffer.from(encImg, 'base64')
-    }
-    agencycollection.insertOne({ title, description, img})
-        .then(result => {
-            console.log(result)
-            res.send(result.insertedCount > 0)
-        })
-});
+    // admin post
+    app.post('/agencyinfo', (req, res) => {
+        const title = req.body.title;
+        const description = req.body.description;
+        const newImg = req.files.file.data;
+        const encImg = newImg.toString('base64');
+        var img = {
+            contentType: req.files.file.mimetype,
+            size: req.files.file.size,
+            img: Buffer.from(encImg, 'base64')
+        }
+        agencycollection.insertOne({ title, description, img })
+            .then(result => {
+                console.log(result)
+                res.send(result.insertedCount > 0)
+            })
+    });
 
-    app.get('/allagency', (req,res)=> {
+    app.get('/allagency', (req, res) => {
         agencycollection.find({})
-        .toArray((err,documents)=> {
-            res.send(documents)
-        })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 
 
@@ -74,80 +72,83 @@ app.post('/agencyinfo', (req, res) => {
             size: req.files.file.size,
             img: Buffer.from(encImg, 'base64')
         };
-        orderCollection.insertOne({ name, email, service, description, price, image,orderImage })
+        orderCollection.insertOne({ name, email, service, description, price, image, orderImage })
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
 
     app.get('/orderserverlist', (req, res) => {
-        orderCollection.find({email: req.query.email})
+        orderCollection.find({ email: req.query.email })
             .toArray((err, document) => {
                 res.send(document);
             })
 
     })
-// admin all order review
-    app.get('/adminAllOrderReview', (req,res)=> {
+    // admin all order review
+    app.get('/adminAllOrderReview', (req, res) => {
         orderCollection.find({})
-        .toArray((err, document) => {
-            res.send(document);
-        })
+            .toArray((err, document) => {
+                res.send(document);
+            })
     })
 
-   // review collection
-   app.post('/allReview', (req, res) => {
-    const name = req.body.name;
-    const designation = req.body.designation;
-    const reviewdescription = req.body.reviewdescription;
-    const newImg = req.files.file.data;
-    const encImg = newImg.toString('base64');
-    var image = {
-        contentType: req.files.file.mimetype,
-        size: req.files.file.size,
-        img: Buffer.from(encImg, 'base64')
-    }
-    reviewCollection.insertOne({ name, designation,image,reviewdescription })
-        .then(result => {
-            console.log(result);
-            res.send(result)
-        })
-})
+    // review collection
+    app.post('/allReview', (req, res) => {
+        const name = req.body.name;
+        const designation = req.body.designation;
+        const reviewdescription = req.body.reviewdescription;
+        const newImg = req.files.file.data;
+        const encImg = newImg.toString('base64');
+        var image = {
+            contentType: req.files.file.mimetype,
+            size: req.files.file.size,
+            img: Buffer.from(encImg, 'base64')
+        }
+        reviewCollection.insertOne({ name, designation, image, reviewdescription })
+            .then(result => {
+                console.log(result);
+                res.send(result)
+            })
+    })
 
     app.get('/getAllReview', (req, res) => {
         reviewCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-        })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+
+    // make Admin
+    app.post("/check-admin", (req, res) => {
+        const email = req.body.email;
+        adminEmailCT.find({ email: email }).toArray((err, doc) => {
+            if (doc.length === 0) {
+                res.send({ admin: false });
+            } else {
+                res.send({ admin: true });
+            }
+        });
+    });
+
+
+    app.post("/addAdmin", (req, res) => {
+        const admin = {
+            email: req.body.email
+        }
+        adminEmailCT.insertOne(admin)
+            .then((result) => {
+                res.json({ adminadd: result.insertedCount > 0 })
+            });
+    });
+
+
 })
 
-
-// make Admin
-app.post("/check-admin", (req, res) => {
-    const email = req.body.email;
-    adminEmailCT.find({ email: email }).toArray((err, doc) => {
-      if (doc.length === 0) {
-        res.send({ admin: false });
-      } else {
-        res.send({ admin: true });
-      }
-    });
-  });
-
-
-  app.post("/addAdmin", (req, res) => {
-    const admin = {
-        email: req.body.email
-    }
-    adminEmailCT.insertOne(admin)
-    .then((result) => {
-      res.json({adminadd : result.insertedCount > 0})
-    });
-  });
-
-
+app.get('/', (req, res) => {
+    res.send("working is done")
 })
-
 
 
 app.listen(process.env.PORT || port)
