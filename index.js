4b7eb8fdbf7f5b4993058c8333bd0cf25fef7da3
wpredoy <rah415704@gmail.com>
2020-10-18 12:21:07 +0600

    done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors")
 const fs = require('fs-extra');
 require('dotenv').config()
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tq72w.mongodb.net/name?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tq72w.mongodb.net/name?retryWrites=true&w=majority`;
 
 
 const app = express();
